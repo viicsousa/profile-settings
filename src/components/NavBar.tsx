2cc6d3140b69921dd4be6353695d1e56329ac113
Victória <victoria.vasconcelos@hotmail.com>
2023-06-01 11:27:55 -0300

    feat: :art: add navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import User from '../assets/svg/user.svg'
 export function NavBar () {
   return (
     <div className="py-4 flex bg-indigo-600 h-20 min-w-max justify-around items-center">
-    <div className="text-gray-50 font-sans text-center">Clique no ícone</div>
+    <div className="text-gray-50 font-sans text-center ">Clique no ícone</div>
     <div className=""><Image src={User} alt="" width={50}/></div>
    </div>
   )
